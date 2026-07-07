c75cb6530590790efa19a4e32dd96ca563862307
Soorya Prakash R <144594996+soorya-prakash-r@users.noreply.github.com>
2026-07-07 19:17:10 +0530

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+        
+        def is_valid(n):
+            summ = 0
+            summ = sum([math.ceil(x/n) for x in piles])
+            if summ <= h:
+                return True
+        res = r
+        while l <= r:
+            mid = math.ceil(l + (r-l)/2)
+            
+            if is_valid(mid):
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return res
